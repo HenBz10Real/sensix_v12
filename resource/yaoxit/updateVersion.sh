80697e60dc63201913cbbf473bd7363d209a3150
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-09-30 01:49:49 +0700

    Update updateVersion.sh

--- resource/yaoxit/updateVersion.sh
+++ resource/yaoxit/updateVersion.sh
@@ -1,6 +1,6 @@
 echo 'export async function setVersion() {
     const versionDiv = document.createElement("div");
     versionDiv.setAttribute("id", "currentVersion");
-    versionDiv.setAttribute("data-version", "1.3");
+    versionDiv.setAttribute("data-version", "1.4");
     document.body.appendChild(versionDiv);
 }'>/sdcard/VortexModules/yaoxit/webroot/version.js
