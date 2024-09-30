8f19412b9dbe1abf7cbb600591906bf67def447c
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-09-30 17:07:43 +0700

    Update versionUpdate.sh

--- resource/webxit/versionUpdate.sh
+++ resource/webxit/versionUpdate.sh
@@ -1,6 +1,6 @@
 echo 'export async function setVersion() {
     const versionDiv = document.createElement("div");
     versionDiv.setAttribute("id", "currentVersion");
-    versionDiv.setAttribute("data-version", "1.7");
+    versionDiv.setAttribute("data-version", "1.8");
     document.body.appendChild(versionDiv);
 }'>/sdcard/VortexModules/webxit/webroot/version.js
